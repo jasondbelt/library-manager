328efe6a49e9b8206d66a8b8b7f7322dd8f4b602
Jason Belt <jasondbelt@gmail.com>
2025-04-01 16:21:16 -0500

    defined book model

--- book_app/models.py
+++ book_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
+    author = models.CharField(max_length=255, null=False, blank=False)
+    isbn = models.CharField(max_length=255, null=False, blank=False)
+    genre = models.CharField(max_length=255, null=False, blank=False)
+    published_date = models.DateField()
+    
+    
+    
